c171defd4781eedc0ab5c492e1c6c9ca0de94459
KaiqueFLH <kaique_homem@estudante.sesisenai.org.br>
2023-06-14 21:50:43 -0300

    ChoiceChords Finalizado

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Variáveis usadas no código
 const nota1 = document.getElementById("nota1");
 const nota2 = document.getElementById("nota2");
 const nota3 = document.getElementById("nota3");
@@ -9,7 +10,7 @@ const modificadorNota3 = document.getElementById("modificadorNota3");
 const acorde = document.getElementById("acorde");
 const modificadorAcorde = document.getElementById("modificadorAcorde");
 
-
+// Definindo valores as variáveis
 acorde.value = "0";
 nota1.value = "0";
 nota2.value = "0";
@@ -21,26 +22,24 @@ modificadorAcorde.value = "0";
 
 let procura;
 
+// Busca o acorde ou a nota para completar o que está faltando
 function buscar() {
-    if (document.getElementById("enviar").lastChild != null) {
-        document.getElementById("enviar").removeChild(document.getElementById("enviar").lastChild);
-    }
-    let erro = document.createElement("p");
-    erro.style.color = "red";
-
     if (this.procura == "Acorde") {
         if (acorde.value != "0") {
             defineNota();
+        }
+        else if(modificadorAcorde.value=="0" &&
+        acorde.value == "0"){
+            document.getElementById("blur").style.display = "flex";
+            document.getElementById("erroAcordeVazio").classList.remove("popupFechar")
+            document.getElementById("erroAcordeVazio").classList.add("popup")
+            document.getElementById("erroAcordeVazio").style.display="flex";
         }
         else if (nota1.value != "0" &&
             nota2.value != "0" &&
             nota3.value != "0") {
             defineAcorde();
 
-        }
-        else {
-            erro.innerText = "Você precisa inserir um acorde válido";
-            document.getElementById("enviar").appendChild(erro)
         }
     }
     if (this.procura == "Nota") {
@@ -49,52 +48,76 @@ function buscar() {
             nota3.value != "0") {
             defineAcorde();
         }
+        else if(nota1.value == "0" &&
+        nota2.value == "0" &&
+        nota3.value == "0" &&
+        modificadorNota1.value == "0" &&
+        modificadorNota2.value == "0" &&
+        modificadorNota3.value == "0"){
+            defineNota();
+        }
         else {
-            erro.innerText = "Você precisa inserir 3 notas válidas";
-            document.getElementById("enviar").appendChild(erro)
+            document.getElementById("blur").style.display = "flex";
+            document.getElementById("erroNotaVazio").classList.remove("popupFechar")
+            document.getElementById("erroNotaVazio").classList.add("popup")
+            document.getElementById("erroNotaVazio").style.display="flex";
         }
     }
     if (this.procura == undefined) {
-        erro.innerText = "Você precisa inserir algo válido";
-        document.getElementById("enviar").appendChild(erro)
+        document.getElementById("blur").style.display = "flex";
+        document.getElementById("erroTudoVazio").classList.remove("popupFechar")
+        document.getElementById("erroTudoVazio").classList.add("popup")
+        document.getElementById("erroTudoVazio").style.display="flex";
     }
 }
 
+// Através das notas definidas procura o acorde que se encaixa
 function procuraAcorde() {
     this.procura = "Acorde";
     if (acorde.value == "3" || acorde.value == "6.5") {
         modificadorAcorde.children[1].disabled = true;
         modificadorAcorde.children[3].disabled = true;
-        modificadorNota1.value = "0";
+        if(modificadorAcorde.value != "0" && modificadorAcorde.value != "m"){
+            modificadorAcorde.value = "0";
+        }
     } else {
         modificadorAcorde.children[1].disabled = false;
         modificadorAcorde.children[3].disabled = false;
     }
 }
+
+// Através do acorde definido procura as notas que se encaixam 
 function procuraNota() {
     this.procura = "Nota";
     if (nota1.value == "3" || nota1.value == "6.5") {
         modificadorNota1.children[1].disabled = true;
-        modificadorNota1.value = "0";
+        if(modificadorNota1.value=="0.5"){
+            modificadorNota1.value = "0";
+        }
     } else {
         modificadorNota1.children[1].disabled = false;
     }
     if (nota2.value == "3" || nota2.value == "6.5") {
         modificadorNota2.children[1].disabled = true;
-        modificadorNota2.value = "0";
+        if(modificadorNota2.value=="0.5"){
+            modificadorNota2.value = "0";
+        }
 
     } else {
         modificadorNota2.children[1].disabled = false;
     }
     if (nota3.value == "3" || nota3.value == "6.5") {
         modificadorNota3.children[1].disabled = true;
-        modificadorNota3.value = "0";
+        if(modificadorNota3.value == "0.5"){
+            modificadorNota3.value = "0";
+        }
 
     } else {
         modificadorNota3.children[1].disabled = false;
     }
 }
 
+// Define o acorde a partir das notas
 function defineAcorde() {
     let teste1 = testePrimeira(nota1, nota2, nota3, modificadorNota1, modificadorNota2, modificadorNota3);
     let teste2 = testePrimeira(nota2, nota1, nota3, modificadorNota2, modificadorNota1, modificadorNota3);
@@ -118,12 +141,15 @@ function defineAcorde() {
     }
     else {
         let erro = document.createElement("p");
-        erro.style.color = "red";
-        erro.innerText = "Esse acorde não é um acorde válido conhecido!";
-        document.getElementById("enviar").appendChild(erro)
+        document.getElementById("blur").style.display = "flex";
+        document.getElementById("erroAcordeInvalido").classList.remove("popupFechar")
+        document.getElementById("erroAcordeInvalido").classList.add("popup")
+
+        document.getElementById("erroAcordeInvalido").style.display="flex";
     }
 }
 
+// Define as notas apartir do acorde
 function defineNota() {
     nota1.value = acorde.value;
     let indiceSegunda = defineIndiceSegunda();
@@ -146,7 +172,7 @@ function defineNota() {
         indiceSegunda -= 6;
     }
     if (indiceTerca > 6.5) {
-        indiceSegunda -= 6;
+        indiceTerca -= 6;
     }
 
     for (let opcao of nota1.children) {
@@ -172,6 +198,7 @@ function defineNota() {
     mudarTeclado();
 }
 
+// testa se a nota é a primeira do acorde
 function testePrimeira(notaA, notaB, notaC, modificadorA, modificadorB, modificadorC) {
     let primeira = JSON.parse(notaA.value) + JSON.parse(modificadorA.value);
     let nota1 = JSON.parse(notaB.value) + JSON.parse(modificadorB.value);
@@ -216,12 +243,12 @@ function testePrimeira(notaA, notaB, notaC, modificadorA, modificadorB, modifica
     return ""
 }
 
+// Muda a cor do teclado através do acorde definido
 function mudarTeclado() {
     let indiceSegunda = defineIndiceSegunda();
     let indiceTerca = defineIndiceTerca();
 
     for (let tecla of document.getElementById("fundoTeclado").children) {
-        console.log("a");
         if (tecla.id == JSON.parse(nota1.value) + JSON.parse(modificadorNota1.value) ||
             tecla.id == indiceSegunda ||
             tecla.id == indiceTerca) {
@@ -236,6 +263,7 @@ function mudarTeclado() {
     }
 }
 
+// Define a segunda nota do acorde  
 function defineIndiceSegunda() {
     if (modificadorAcorde.value == "#") {
         return JSON.parse(nota1.value) + 2.5;
@@ -251,6 +279,7 @@ function defineIndiceSegunda() {
     }
 }
 
+// Define a terceira nota do acorde
 function defineIndiceTerca() {
     if (modificadorAcorde.value == "#") {
         return JSON.parse(nota1.value) + 4;
@@ -267,10 +296,27 @@ function defineIndiceTerca() {
 
 }
 
-function abrirPopup(){
-    document.getElementById("defoco").style.display = "flex";
+
+// Função para abrir e fechar Popup
+function abrirPopup() {
+    document.getElementById("blur").style.display = "flex";
+    document.getElementById("instrucoes").classList.remove("popupFechar")
+    document.getElementById("instrucoes").classList.add("popup")
+
+    document.getElementById("instrucoes").style.display="flex";
 }
-function fechar(){
-    document.getElementById("defoco").style.display = "none";
-    
+function fechar(id) {
+
+    document.getElementById(id).classList.remove("popup")
+    document.getElementById(id).classList.add("popupFechar")
+
+
+    setTimeout(() => {
+        document.getElementById("blur").style.display = "none";
+        document.getElementById(id).style.display = "none";
+    }, "250");
+
+
 }
+
+
